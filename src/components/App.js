70e3881dca58ecdc98ea7d77b7d8461b730b3d34
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-10-26 10:08:45 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,43 @@ import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  let [x, setX] = useState(0);
+  let [y, setY] = useState(0);
   const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px"
   });
   const reset = () => {
-    setRenderBall({ renderBall: false });
-    setX({ x: 0 });
-    setY({ y: 0 });
-    setBallPosition({ ballPosition: { left: "0px", top: "0px" } });
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: "0px" });
   };
   const handleStart = (event) => {
     setRenderBall(true);
     document.addEventListener("keydown", handleKeyPress);
   };
   const handleKeyPress = (event) => {
+    console.log(event.key);
     if (event.keyCode === 39) {
       setX({ x: x + 5 });
       setBallPosition({
-        ballPosition: { left: `${x}px` }
+        ballPosition: { left: `${x}px`, top: `${y}px` }
       });
     } else if (event.keyCode === 40) {
       setY(y - 5);
       setBallPosition({
-        ballPosition: { top: `${y}px` }
+        ballPosition: { left: `${x}px`, top: `${y}px` }
       });
     } else if (event.keyCode === 38) {
       setY(y + 5);
       setBallPosition({
-        ballPosition: { top: `${y}px` }
+        ballPosition: { left: `${x}px`, top: `${y}px` }
       });
     } else if (event.keyCode === 37) {
       setX(x - 5);
       setBallPosition({
-        ballPosition: { lest: `${x}px` }
+        ballPosition: { left: `${x}px`, top: `${y}px` }
       });
     }
   };
